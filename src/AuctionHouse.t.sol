afa3279770a17d0521ca389c781f3b2666c61395
sauron <eyeofsauron_seen@protonmail.com>
2021-01-20 16:29:51 -0500

    Update to 8.0

--- src/AuctionHouse.t.sol
+++ src/AuctionHouse.t.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.6.7;
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.0;
 
 import "ds-test/test.sol";
 
--- src/EnglishAuction.sol
+++ src/EnglishAuction.sol
@@ -2,5 +2,5 @@
 
 pragma solidity ^0.8.0;
 
-contract AuctionHouse {
+contract EnglishAuction {
 }
