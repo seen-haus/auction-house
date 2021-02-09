8b3f47d860f212c3d4db0fc849ef08648e45a0dc
sauron <eyeofsauron_seen@protonmail.com>
2021-02-09 13:23:45 -0500

    Update to governable token

--- src/SeenHaus.sol
+++ src/SeenHaus.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.0;
 
-import "./OpenZeppelin/ERC20/ERC20.sol";
+import "./OpenZeppelin/ERC20/IERC20.sol";
+import "./Governance/GovernableERC20.sol";
 
 interface IWETH {
     function deposit() external payable;
@@ -17,7 +18,7 @@ interface Sushiswap {
     ) external returns (uint256[] memory amounts);
 }
 
-contract SeenHaus is ERC20("SeenHaus", "xSEEN"){
+contract SeenHaus is GovERC20(){
     address public constant weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public constant sushiswap = 0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F;
     IERC20 public constant seen = IERC20(0xCa3FE04C7Ee111F0bbb02C328c699226aCf9Fd33);
@@ -29,14 +30,14 @@ contract SeenHaus is ERC20("SeenHaus", "xSEEN"){
         IERC20(weth).approve(sushiswap, type(uint256).max);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal view override {
+    function _beforeTokenTransfer(address from) internal view override {
         require(locked[from] <= block.timestamp, "transfer:too soon after minting");
     }
 
     // Enter the haus. Pay some SEENs. Earn some shares.
     function enter(uint256 _amount) public {
         uint256 totalSeen = seen.balanceOf(address(this));
-        uint256 totalShares = totalSupply();
+        uint256 totalShares = totalSupply;
 
         locked[msg.sender] = block.timestamp + 3 days;
 
@@ -51,7 +52,7 @@ contract SeenHaus is ERC20("SeenHaus", "xSEEN"){
 
     // Leave the haus. Claim back your SEENs.
     function leave(uint256 _share) public {
-        uint256 totalShares = totalSupply();
+        uint256 totalShares = totalSupply;
         uint256 what = _share * seen.balanceOf(address(this)) / totalShares;
         _burn(msg.sender, _share);
         seen.transfer(msg.sender, what);
